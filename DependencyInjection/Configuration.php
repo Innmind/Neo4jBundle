5bed2eccc59721137e70899b890676bd13cdfae0
Baptiste Langlade <baptouuuu@gmail.com>
2015-06-25 08:02:16 +0200

    add basic repo files

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Innmind\Neo4jBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder;
+        $treeBuilder->root('innmind_neo4j');
+
+        return $treeBuilder;
+    }
+}
---
+++ InnmindNeo4jBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Innmind\Neo4jBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InnmindNeo4jBundle extends Bundle
+{
+
+}
