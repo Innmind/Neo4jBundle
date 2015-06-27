484ca01a1fa6cdeccb4f7b2d46272916e059cb44
Baptiste Langlade <baptouuuu@gmail.com>
2015-06-27 18:53:48 +0200

    CS

--- DependencyInjection/InnmindNeo4jExtension.php
+++ DependencyInjection/InnmindNeo4jExtension.php
@@ -21,7 +21,10 @@ class InnmindNeo4jExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
         $loader->load('services.yml');
 
         foreach ($config['connections'] as $name => &$connection) {
