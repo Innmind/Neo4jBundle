b1f91b454dac6a45b043b673550b7b2b5596f467
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-01 08:38:11 +0200

    remove old implementation

--- InnmindNeo4jBundle.php
+++ InnmindNeo4jBundle.php
@@ -2,8 +2,6 @@
 
 namespace Innmind\Neo4jBundle;
 
-use Innmind\Neo4jBundle\DependencyInjection\Compiler\ComputeBundlesPathPass;
-use Innmind\Neo4jBundle\DependencyInjection\Compiler\RegisterManagersPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -12,13 +10,5 @@ class InnmindNeo4jBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
-        $container
-            ->addCompilerPass(
-                new ComputeBundlesPathPass
-            )
-            ->addCompilerPass(
-                new RegisterManagersPass
-            );
     }
 }
