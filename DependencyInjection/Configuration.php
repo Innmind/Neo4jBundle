5360a914995ee3ba65fcc80ddde2ad1bbc2dade7
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-04 08:05:42 +0200

    add bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,6 +16,40 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder;
         $root = $treeBuilder->root('innmind_neo4j');
 
+        $root
+            ->children()
+                ->arrayNode('connection')
+                    ->children()
+                        ->scalarNode('host')
+                            ->defaultValue('localhost')
+                        ->end()
+                        ->scalarNode('scheme')
+                            ->defaultValue('https')
+                        ->end()
+                        ->integerNode('port')
+                            ->defaultValue(7474)
+                        ->end()
+                        ->scalarNode('user')
+                            ->defaultValue('neo4j')
+                        ->end()
+                        ->scalarNode('password')
+                            ->defaultValue('neo4j')
+                        ->end()
+                        ->integerNode('timeout')
+                            ->defaultValue(60)
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('types')
+                    ->defaultValue([])
+                    ->prototype('scalar')->end()
+                ->end()
+                ->scalarNode('persister')
+                    ->info('The service name to use in the unit of work to persist the entity container')
+                    ->defaultValue('innmind_neo4j.persister.delegation')
+                ->end()
+            ->end();
+
         return $treeBuilder;
     }
 }
