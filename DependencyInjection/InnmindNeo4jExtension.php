2b41077fb1e2c1e56f04f7d549346693629904a7
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-05 07:27:18 +0200

    remove useless use statements

--- DependencyInjection/InnmindNeo4jExtension.php
+++ DependencyInjection/InnmindNeo4jExtension.php
@@ -2,17 +2,11 @@
 
 namespace Innmind\Neo4jBundle\DependencyInjection;
 
-use Innmind\Neo4j\ONM\EntityManagerFactory;
-use Innmind\Neo4j\ONM\EntityManager;
-use Innmind\Neo4j\ONM\Configuration as Conf;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\Parameter;
-use Symfony\Component\DependencyInjection\Alias;
 
 class InnmindNeo4jExtension extends Extension
 {
